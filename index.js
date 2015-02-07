d66b19dbf021f084f59b10ae892a3880020368d8
tunderdomb <tunderdomb@gmail.com>
2015-02-07 13:53:45 +0100

    api experiment

--- index.js
+++ index.js
@@ -1,50 +1,12 @@
-var createPrototype = require("./lib/createPrototype")
-var createAttributes = require("./lib/createAttributes")
-var createDispatcher = require("./lib/createDispatcher")
+var register = require("./lib/register")
+var delegate = require("./lib/delegate")
+var fragment = require("./lib/fragment")
+
 var fidget = {}
+
+fidget.register = register
+fidget.delegate = delegate
+fidget.fragment = fragment
 
 module.exports = fidget
 global.fidget = fidget
-
-var widgets = {}
-
-fidget.widgets = widgets
-
-fidget.register = function( name, base, is, proto ){
-  base = base ? base.prototype || base : window.HTMLElement
-  is = is ? is : undefined
-  proto = proto || {}
-
-  var initializedCallback = proto.initializedCallback
-  delete proto.initializedCallback
-  var attributes = proto.attributes
-  delete proto.attributes
-  var events = proto.events
-  delete proto.events
-
-  createAttributes(proto, attributes)
-  proto.dispatch = createDispatcher(events)
-  proto = createPrototype(proto)
-  proto = Object.create(base, proto)
-
-  var Constructor = window.document.registerElement(name, {
-    'extends': is,
-    prototype: proto
-  })
-
-  function render(){
-    var element = window.document.createElement(name)
-    if( initializedCallback ){
-      initializedCallback.apply(element, arguments)
-    }
-    return element
-  }
-
-  Constructor.render = render
-  widgets[name] = Constructor
-  return Constructor
-}
-
-fidget.render = function( name ){
-  return widgets[name].render(null, [].slice.call(arguments, 1))
-}
