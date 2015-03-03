2800ff3d510487e427b7ab0f5cbb4e5a8d39d516
tunderdomb <tunderdomb@gmail.com>
2015-03-03 23:23:58 +0100

    prototype augmenting (mixins)

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-module.exports.register = require("./lib/register")
-module.exports.delegate = require("./lib/delegate")
-module.exports.fragment = require("./lib/fragment")
+var register = require("./lib/register")
+var delegate = require("./lib/delegate")
+var fragment = require("./lib/fragment")
+
+var fidget = {}
+fidget.register = register
+fidget.delegate = delegate
+fidget.fragment = fragment
+
+module.exports = fidget
 
 // we're browserified
 if( typeof global != "undefined" && typeof window != "undefined" && global == window){
   // set a global reference for dist scripts
-  window.fidget = module.exports
+  window.fidget = fidget
 }
