6d7d671e102e816767673131a441b974802e2f7e
tunderdomb <tunderdomb@gmail.com>
2015-04-23 09:38:53 +0200

    components helpers + build script

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var register = require("./lib/register")
 var delegate = require("./lib/delegate")
 var fragment = require("./lib/fragment")
+var components = require("./lib/components")
 
 var fidget = {}
 fidget.register = register
 fidget.delegate = delegate
 fidget.fragment = fragment
+fidget.components = components
 
 module.exports = fidget
 
 // we're browserified
-if( typeof global != "undefined" && typeof window != "undefined" && global == window){
+if( typeof global != "undefined" && typeof window != "undefined" && global == window ){
   // set a global reference for dist scripts
   window.fidget = fidget
 }
